0b3431e6492d48e07fa068b9a197f31c4d490ed2
yigit1232 <yigit_ali_ucun@outlook.com>
2023-05-18 20:26:30 +0300

    31

--- core/settings.py
+++ core/settings.py
@@ -102,7 +102,7 @@ STATICFILES_DIRS=[
 STATIC_ROOT = [
     BASE_DIR / 'staticfiles'
 ]
-
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
