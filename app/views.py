c84bb79274c954717c7223e4a8105cb3f91eb3bd
yigit1232 <yigit_ali_ucun@outlook.com>
2023-05-23 18:25:27 +0300

    add send email

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .data import get_images
+from django.core.mail import send_mail
+from django.conf import settings
 def index(request):
     return render(request,'index.html')
 
@@ -10,6 +12,34 @@ def quality(request):
     return render(request, 'quality.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject= request.POST.get('subject')
+        fax = request.POST.get('fax')
+        phone = request.POST.get('phone')
+        mobile_phone = request.POST.get('mobile_phone')
+        website = request.POST.get('website')
+        company = request.POST.get('company')
+        send_mail(
+            subject,
+            f"""
+            Konu: {subject}
+            
+            
+            
+            İsim Soyisim: {name}
+            Email: {email}
+            Faks: {fax}
+            Telefon: {phone}
+            Cep Telefonu: {mobile_phone}
+            Web Sitesi: {website}
+            Şirket: {company}
+            """,
+            settings.EMAIL_HOST_USER,
+            [settings.EMAIL_HOST_USER]
+        )
+        return render(request, 'contact.html')
     return render(request, 'contact.html')
 
 def production(request):
--- core/settings.py
+++ core/settings.py
@@ -79,6 +79,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.office365.com'  # E-posta sağlayıcınızın SMTP sunucusu
+EMAIL_PORT = 587  # E-posta sağlayıcınızın SMTP portu
+EMAIL_USE_TLS = True  # TLS (Transport Layer Security) kullanıyor mu?
+EMAIL_HOST_USER = 'yigit_ali_ucun@hotmail.com'  # E-posta adresiniz
+EMAIL_HOST_PASSWORD = 'Catapata4321'  # E-posta şifreniz
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
