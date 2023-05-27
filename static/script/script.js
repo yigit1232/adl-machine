36f2dc585da65892dd2f73d7f96f80dcb205f65c
yigit1232 <yigit_ali_ucun@outlook.com>
2023-05-27 15:22:39 +0300

    css bug fix

--- static/script/script.js
+++ static/script/script.js
@@ -1,17 +1,4 @@
 
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 0,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
 
 $(".menu").on("click",function(){
     $(".menu-content").toggle()
